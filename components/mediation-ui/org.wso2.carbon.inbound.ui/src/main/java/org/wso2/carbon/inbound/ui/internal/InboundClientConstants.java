51d8ae927739fb16dc2faf5e058a8f57b5263fb7
malakasilva <malaka@wso2.com>
2014-07-16 16:38:07 +0530

    Inbound UI

---
+++ components/mediation-ui/org.wso2.carbon.inbound.ui/src/main/java/org/wso2/carbon/inbound/ui/internal/InboundClientConstants.java
@@ -0,0 +1,11 @@
+package org.wso2.carbon.inbound.ui.internal;
+
+public class InboundClientConstants {
+
+	public static final String TYPE_FILE = "FILE";
+	public static final String TYPE_JMS = "JMS";
+	public static final String TYPE_CLASS = "CLASS";
+	
+	public static final String EXCEPTION = "INBOUND_EXCEPTION";
+	
+}
