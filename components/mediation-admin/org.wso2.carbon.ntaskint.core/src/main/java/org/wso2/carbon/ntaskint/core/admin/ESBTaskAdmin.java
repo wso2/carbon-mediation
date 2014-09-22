ca8fc2606929201053bf80f0f8c646ae0534b23c
chanakaf@wso2.com <chanakaf@wso2.com>
2014-09-22 11:14:51 +0530

    Adding changes required for implementing NTask Admin service for IPAAS dashboard

--- components/mediation-admin/org.wso2.carbon.ntaskint.core/src/main/java/org/wso2/carbon/ntaskint/core/admin/ESBTaskAdmin.java
+++ components/mediation-admin/org.wso2.carbon.ntaskint.core/src/main/java/org/wso2/carbon/ntaskint/core/admin/ESBTaskAdmin.java
@@ -1,8 +1,21 @@
 package org.wso2.carbon.ntaskint.core.admin;
 
 import org.wso2.carbon.core.AbstractAdmin;
+import org.wso2.carbon.mediation.initializer.AbstractServiceBusAdmin;
+import org.wso2.carbon.mediation.ntask.NTaskTaskManager;
+import org.wso2.carbon.ntask.core.impl.remote.RemoteTaskManager;
 
-public class ESBTaskAdmin extends AbstractAdmin {
+import java.util.List;
+
+public class ESBTaskAdmin extends AbstractServiceBusAdmin {
+
+    private NTaskTaskManager ntaskManager;
+
+    public ESBTaskAdmin() {
+        ntaskManager = (NTaskTaskManager)getSynapseConfiguration().getTaskManager();
+    }
+
+
     public void addRemoteTask(/*StaticTaskInformation taskInfo*/) throws Exception {
         //RemoteTaskManager.getInstance().addTask(taskInfo);
     }
@@ -45,7 +58,7 @@ public class ESBTaskAdmin extends AbstractAdmin {
     }
 
     public /*DeployedTaskInformation*/ void getRemoteSystemTask(String name,
-                                                       int targetTenantId) throws Exception {
+                                                                int targetTenantId) throws Exception {
         try {
 //            return RemoteTaskManager.getInstance().getSystemTask(targetTenantId, name);
         } catch (Exception e) {
@@ -74,5 +87,44 @@ public class ESBTaskAdmin extends AbstractAdmin {
         //return RemoteTaskManager.getInstance().getAllSystemTasks(targetTenantId);
         return null;
     }
+
+    //Adding ESB specific task operations
+
+    public List<String> getRunningESBTaskList() throws Exception {
+        return ntaskManager.getRunningTaskList();
+    }
+
+    public boolean isESBTaskRunning(String taskName) throws Exception {
+        return ntaskManager.isTaskRunning(taskName);
+    }
+
+    public int getRunningESBTaskCount() throws Exception {
+        return ntaskManager.getRunningTaskCount();
+    }
+
+    public boolean deleteESBTask(String name) throws Exception {
+        return ntaskManager.delete(name);
+    }
+
+    public boolean pauseESBTask(String name) throws Exception {
+        return ntaskManager.pause(name);
+    }
+
+    public boolean resumeESBTask(String name) throws Exception {
+        return ntaskManager.resume(name);
+    }
+
+    public boolean pauseAllESBTasks() throws Exception {
+        return ntaskManager.pauseAll();
+    }
+
+    public boolean resumeAllESBTasks() throws Exception {
+        return ntaskManager.resumeAll();
+    }
+
+    public String[] getESBTaskNames() throws Exception {
+        return ntaskManager.getTaskNames();
+    }
+
 
 }
