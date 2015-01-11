cc1c3080535ab9f07e8b31c5d33e15d3a4f869d9
Maheeka Jayasuriya <maheeka@wso2.com>
2015-01-12 04:31:18 +0530

    format

--- components/mediation-initializer/org.wso2.carbon.mediation.dependency.mgt/src/main/java/org/wso2/carbon/mediation/dependency/mgt/DependencyResolverFactory.java
+++ components/mediation-initializer/org.wso2.carbon.mediation.dependency.mgt/src/main/java/org/wso2/carbon/mediation/dependency/mgt/DependencyResolverFactory.java
@@ -21,7 +21,6 @@ import org.wso2.carbon.mediators.router.impl.RouterMediator;
 import org.apache.synapse.mediators.builtin.SendMediator;
 import org.apache.synapse.mediators.builtin.CalloutMediator;
 import org.apache.synapse.mediators.builtin.ValidateMediator;
-import org.apache.synapse.mediators.builtin.ForEachMediator;
 import org.apache.synapse.mediators.builtin.CacheMediator;
 import org.apache.synapse.mediators.base.SequenceMediator;
 import org.apache.synapse.mediators.xquery.XQueryMediator;
