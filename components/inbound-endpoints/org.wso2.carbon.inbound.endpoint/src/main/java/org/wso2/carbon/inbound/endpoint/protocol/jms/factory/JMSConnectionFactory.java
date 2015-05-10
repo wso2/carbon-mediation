35927fad3b4877297ffc6a55b9de4741cafe2f73
ErandaRajapakshe <erandacr@gmail.com>
2015-05-10 21:44:15 +0530

    Implemented new abstract methods in 2.0

--- components/inbound-endpoints/org.wso2.carbon.inbound.endpoint/src/main/java/org/wso2/carbon/inbound/endpoint/protocol/jms/factory/JMSConnectionFactory.java
+++ components/inbound-endpoints/org.wso2.carbon.inbound.endpoint/src/main/java/org/wso2/carbon/inbound/endpoint/protocol/jms/factory/JMSConnectionFactory.java
@@ -275,7 +275,8 @@ public class JMSConnectionFactory implements ConnectionFactory, QueueConnectionF
 
     public QueueConnection createQueueConnection(String userName, String password) throws JMSException {
         try {
-            return ((QueueConnectionFactory) (this.connectionFactory)).createQueueConnection(userName, password);
+            return ((QueueConnectionFactory) (this.connectionFactory)).createQueueConnection(
+                    userName, password);
         } catch (JMSException e) {
             logger.error("JMS Exception while creating queue connection through factory '" + this.connectionFactoryString + "' " + e.getMessage());
         }
@@ -449,18 +450,20 @@ public class JMSConnectionFactory implements ConnectionFactory, QueueConnectionF
 		return sessionAckMode;
 	}
 
-    public javax.jms.JMSContext createContext(int sessionMode){
-        return null;
-    }
-    public javax.jms.JMSContext createContext(String userName, String password, int sessionMode){
-        return null;
+    public javax.jms.JMSContext createContext() {
+        return connectionFactory.createContext();
     }
 
-    public javax.jms.JMSContext createContext(String userName, String password){
-        return null;
+    public javax.jms.JMSContext createContext(int sessionMode) {
+        return connectionFactory.createContext(sessionMode);
     }
 
-    public javax.jms.JMSContext createContext(){
-        return null;
+    public javax.jms.JMSContext createContext(String userName, String password) {
+        return connectionFactory.createContext(userName, password);
     }
+
+    public javax.jms.JMSContext createContext(String userName, String password, int sessionMode) {
+        return connectionFactory.createContext(userName, password, sessionMode);
+    }
+
 }
