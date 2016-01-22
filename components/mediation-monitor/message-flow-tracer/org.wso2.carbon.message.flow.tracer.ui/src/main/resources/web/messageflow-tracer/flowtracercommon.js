1a7c7ac39ab46770e475ebc89a70f8012ed354b8
Buddhima <buddhimawijeweera@yahoo.com>
2016-01-23 01:32:44 +0530

    Remove FE UI in management console

--- components/mediation-monitor/message-flow-tracer/org.wso2.carbon.message.flow.tracer.ui/src/main/resources/web/messageflow-tracer/flowtracercommon.js
+++
@@ -1,7 +0,0 @@
-function flowDetails(messageID) {
-    document.location.href = "messageflowdetails.jsp?" + "messageid=" + messageID;
-}
-
-function clearAllNew() {
-    document.location.href = "index.jsp?op=clear";
-}
