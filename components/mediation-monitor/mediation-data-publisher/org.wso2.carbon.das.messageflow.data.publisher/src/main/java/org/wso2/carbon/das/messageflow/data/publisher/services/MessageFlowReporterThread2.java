fc6e23304e6461f744979c8793c34eef7d4bf1ff
virajsenevirathne <viraj.uom@gmail.com>
2016-07-28 00:54:37 +0530

    Working Publishing Flow

--- components/mediation-monitor/mediation-data-publisher/org.wso2.carbon.das.messageflow.data.publisher/src/main/java/org/wso2/carbon/das/messageflow/data/publisher/services/MessageFlowReporterThread2.java
+++ components/mediation-monitor/mediation-data-publisher/org.wso2.carbon.das.messageflow.data.publisher/src/main/java/org/wso2/carbon/das/messageflow/data/publisher/services/MessageFlowReporterThread2.java
@@ -17,16 +17,26 @@
 */
 package org.wso2.carbon.das.messageflow.data.publisher.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.log4j.Logger;
+import org.apache.synapse.aspects.flow.statistics.data.raw.BasicStatisticDataUnit;
+import org.apache.synapse.aspects.flow.statistics.data.raw.CallbackDataUnit;
+import org.apache.synapse.aspects.flow.statistics.data.raw.StatisticDataUnit;
+import org.apache.synapse.aspects.flow.statistics.data.raw.StatisticsLog;
 import org.apache.synapse.aspects.flow.statistics.log.MessageFlowProcessorInterface;
 import org.apache.synapse.aspects.flow.statistics.log.StatisticsReportingEvent;
 import org.apache.synapse.aspects.flow.statistics.log.StatisticsReportingEventHolder;
+import org.apache.synapse.aspects.flow.statistics.log.templates.AbstractStatisticEvent;
 import org.apache.synapse.aspects.flow.statistics.publishing.PublishingFlow;
 import org.apache.synapse.aspects.flow.statistics.store.CompletedStatisticStore;
+import org.apache.synapse.aspects.flow.statistics.util.TracingDataCollectionHelper;
 import org.wso2.carbon.das.messageflow.data.publisher.data.MessageFlowObserverStore;
 import org.wso2.carbon.mediation.initializer.services.SynapseEnvironmentService;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class MessageFlowReporterThread2 extends Thread {
     private static Logger log = Logger.getLogger(MessageFlowReporterThread2.class);
@@ -36,13 +46,15 @@ public class MessageFlowReporterThread2 extends Thread {
 
     private MessageFlowObserverStore messageFlowObserverStore;
 
-    /** The reference to the synapse environment service */
+    /**
+     * The reference to the synapse environment service
+     */
     private SynapseEnvironmentService synapseEnvironmentService;
 
     private long delay = 5000;
 
 
-	public MessageFlowReporterThread2(SynapseEnvironmentService synEnvSvc,
+    public MessageFlowReporterThread2(SynapseEnvironmentService synEnvSvc,
                                       MessageFlowObserverStore messageFlowObserverStore) {
         this.synapseEnvironmentService = synEnvSvc;
         this.messageFlowObserverStore = messageFlowObserverStore;
@@ -73,68 +85,144 @@ public class MessageFlowReporterThread2 extends Thread {
             try {
                 statisticsReportingEventHolder = synapseEnvironmentService.getSynapseEnvironment().getMessageDataStore().dequeue();
                 if (statisticsReportingEventHolder != null) {
-                    MessageFlowProcessorInterface flowProcessor = new MessageFlowProcessor(messageFlowObserverStore);
-                    log.info("******* event list size - " + statisticsReportingEventHolder.getEventList().size() + " holder count - " + statisticsReportingEventHolder.countHolder.getStatCount() + " callback - " + statisticsReportingEventHolder.countHolder.getCallBackCount());
-                    for (StatisticsReportingEvent event : statisticsReportingEventHolder.getEventList()) {
-                        event.processEvents(flowProcessor);
-                    }
+
+                    log.info("******* event list size - " + statisticsReportingEventHolder.getQueueSize() + " holder " +
+                            "count - " + statisticsReportingEventHolder.countHolder.getStatCount() + " callback - " + statisticsReportingEventHolder.countHolder.getCallBackCount());
+                    processAndPublishEventList(statisticsReportingEventHolder.getEventList());
                 } else {
-                    Thread.sleep(1);
+                    Thread.sleep(1000);
                 }
             } catch (Exception exception) {
                 log.error("Error in mediation flow statistic data consumer while consuming data", exception);
             }
-
-
-
-
-//            try {
-//                if(log.isDebugEnabled()) {
-//                    log.debug("Executing reporter thread - " + this.getName());
-//                }
-//                collectDataAndReport();
-//                delay();
-//            } catch (Throwable t) {
-//                log.error("Error while collecting and reporting mediation statistics", t);
-//            }
         }
     }
 
-//    private void collectDataAndReport(){
-//	    if (log.isDebugEnabled()) {
-//		    log.trace("Starting new mediation statistics collection cycle");
-//	    }
-//
-//        CompletedStatisticStore completedStatisticStore = synapseEnvironmentService.getSynapseEnvironment().getCompletedStatisticStore();
-//
-//        if (completedStatisticStore == null) {
-//		    if (log.isDebugEnabled()) {
-//                log.debug("Statistics collector is not available in the Synapse environment");
-//		    }
-//            delay();
-//		    return;
-//	    }
-//
-//	    try {
-//            if (!completedStatisticStore.isEmpty()) {
-//
-//                List<PublishingFlow> publishingFlowList = completedStatisticStore.getCompletedStatisticEntries();
-//
-//                for (PublishingFlow aPublishingFlow : publishingFlowList) {
-//                    messageFlowObserverStore.notifyObservers(aPublishingFlow);
-//		    }
-//
-//            }
-//
-//	    } catch (Exception e) {
-//		    log.error("Error while obtaining statistic data.", e);
-//	    }
-//    }
+    private void processAndPublishEventList(List<StatisticsReportingEvent> eventList) {
+
+
+
+        List<StatisticsReportingEvent> remainingEvents = new ArrayList<>();
+        List<StatisticsLog> messageFlowLogs = new ArrayList<>();
+
+        for (StatisticsReportingEvent event : eventList) {
+            if (event.getEventType() == AbstractStatisticEvent.EventType.STATISTICS_OPEN_EVENT) {
+                StatisticsLog statisticsLog = new StatisticsLog((StatisticDataUnit) event.getDataUnit());
+                messageFlowLogs.add(statisticsLog.getCurrentIndex(), statisticsLog);
+            } else {
+                remainingEvents.add(event);
+            }
+        }
+
+        for (StatisticsReportingEvent event:remainingEvents) {
+
+            switch (event.getEventType()){
+                case STATISTICS_CLOSE_EVENT:
+
+                    StatisticDataUnit dataUnit = (StatisticDataUnit) event.getDataUnit();
+
+                    StatisticsLog statisticsLog = messageFlowLogs.get(dataUnit.getCurrentIndex());
+
+                    int parentIndex = statisticsLog.getParentIndex();
+                    if (parentIndex == -1) {
+                        //statisticsLog.setParentIndex(parentIndex);
+                    }else {
+                        statisticsLog.setParentIndex(getParent(messageFlowLogs, parentIndex));
+                    }
+                    statisticsLog.setEndTime(dataUnit.getTime());
+                    statisticsLog.setAfterPayload(dataUnit.getPayload());
+                    updateParents(messageFlowLogs, statisticsLog.getParentIndex(), dataUnit.getTime());
+                    break;
+                case CALLBACK_COMPLETION_EVENT:
+                    CallbackDataUnit callbackDataUnit = (CallbackDataUnit) event.getDataUnit();
+                    if (!callbackDataUnit.isOutOnlyFlow()) {
+                        updateParents(messageFlowLogs, callbackDataUnit.getCurrentIndex(), callbackDataUnit.getTime());
+                    }
+                    break;
+                case CALLBACK_RECEIVED_EVENT:
+                    callbackDataUnit = (CallbackDataUnit) event.getDataUnit();
+                    if (!callbackDataUnit.isOutOnlyFlow()) {
+                        updateParents(messageFlowLogs, callbackDataUnit.getCurrentIndex(), callbackDataUnit.getTime());
+                    }
+                    break;
+                case ENDFLOW_EVENT:
+                    break;
+                case FAULT_EVENT:
+                    BasicStatisticDataUnit basicDataUnit = event.getDataUnit();
+                    addFaultsToParents(messageFlowLogs,basicDataUnit.getCurrentIndex());
+                    break;
+                case PARENT_REOPEN_EVENT:
+                    break;
+                default:
+                    break;
+            }
+            //messageFlowLogs.get(0).setEndTime(eventList.get(event.));
+
+
+        }
+
+        PublishingFlow publishingFlow = TracingDataCollectionHelper.createPublishingFlow(messageFlowLogs);
+        logEvent(publishingFlow);
+        messageFlowObserverStore.notifyObservers(publishingFlow);
+
+
+        log.info("came");
+
+
+
+        //messageFlowObserverStore.notifyObservers(statisticsEntry.getMessageFlowLogs());
+    }
+
+    void updateParents(List<StatisticsLog> messageFlowLogs, int index, long endTime){
+        while(index > -1){
+            StatisticsLog dataUnit = messageFlowLogs.get(index);
+            dataUnit.setEndTime(endTime);
+            index = dataUnit.getParentIndex();
+        }
+    }
+
+    private int getParent(List<StatisticsLog> messageFlowLogs, int parentIndex) {
+        int trueParentIndex = 0;
+        while (parentIndex > -1) {
+            StatisticsLog updatingLog = messageFlowLogs.get(parentIndex);
+            if (updatingLog.getEndTime() == -1) {
+                trueParentIndex = updatingLog.getCurrentIndex();
+                break;
+            }
+            parentIndex = updatingLog.getParentIndex();
+        }
+        return trueParentIndex;
+    }
+
+    void addFaultsToParents(List<StatisticsLog> messageFlowLogs, int index){
+        while(index > -1){
+            StatisticsLog updatingLog = messageFlowLogs.get(index);
+            updatingLog.incrementNoOfFaults();
+            index = updatingLog.getParentIndex();
+        }
+
+    }
 
     public void shutdown() {
         if (log.isDebugEnabled()) {
             log.debug("Statistics reporter thread is being stopped");
         }
         shutdownRequested = true;
+    }
+
+    //todo this is only for test, please remove this before committing - rajith
+    private static void logEvent(PublishingFlow publishingFlow) {
+        Map<String, Object> mapping = publishingFlow.getObjectAsMap();
+        mapping.put("host", "localhost"); // Adding host
+        mapping.put("tenantId", "1234");
+
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = null;
+        try {
+            jsonString = mapper.writeValueAsString(mapping);
+        } catch (JsonProcessingException e) {
+            log.error("Unable to convert", e);
+        }
+        log.info("Uncompressed data -------------------------- : " + jsonString);
     }
 }
