c019bb085e5be2d5c310567b3dae9298cef64601
Vijitha Ekanayake <ekvijitha@gmail.com>
2016-09-08 15:46:38 +0530

    Reformatting whole class to WSO2 code template

--- components/mediation-ui/org.wso2.carbon.event.sink.config.ui/src/main/java/org/wso2/carbon/event/sink/config/ui/PublishEventMediatorConfigAdminClient.java
+++ components/mediation-ui/org.wso2.carbon.event.sink.config.ui/src/main/java/org/wso2/carbon/event/sink/config/ui/PublishEventMediatorConfigAdminClient.java
@@ -42,85 +42,85 @@ import java.util.ResourceBundle;
  */
 public class PublishEventMediatorConfigAdminClient {
 
-	private static final Log log = LogFactory.getLog(PublishEventMediatorConfigAdminClient.class);
-	private static final String BUNDLE = "org.wso2.carbon.event.sink.config.ui.i18n.Resources";
-	private PublishEventMediatorConfigAdminStub stub;
-	private ResourceBundle bundle;
+    private static final Log log = LogFactory.getLog(PublishEventMediatorConfigAdminClient.class);
+    private static final String BUNDLE = "org.wso2.carbon.event.sink.config.ui.i18n.Resources";
+    private PublishEventMediatorConfigAdminStub stub;
+    private ResourceBundle bundle;
 
-	public PublishEventMediatorConfigAdminClient(String cookie, String backendServerURL,
-	                                             ConfigurationContext configCtx, Locale locale) throws AxisFault {
-		String serviceURL = backendServerURL + "PublishEventMediatorConfigAdmin";
-		bundle = ResourceBundle.getBundle(BUNDLE, locale);
+    public PublishEventMediatorConfigAdminClient(String cookie, String backendServerURL,
+                                                 ConfigurationContext configCtx, Locale locale) throws AxisFault {
+        String serviceURL = backendServerURL + "PublishEventMediatorConfigAdmin";
+        bundle = ResourceBundle.getBundle(BUNDLE, locale);
 
-		stub = new PublishEventMediatorConfigAdminStub(configCtx, serviceURL);
-		ServiceClient client = stub._getServiceClient();
-		Options option = client.getOptions();
-		option.setManageSession(true);
-		option.setProperty(org.apache.axis2.transport.http.HTTPConstants.COOKIE_STRING, cookie);
-	}
+        stub = new PublishEventMediatorConfigAdminStub(configCtx, serviceURL);
+        ServiceClient client = stub._getServiceClient();
+        Options option = client.getOptions();
+        option.setManageSession(true);
+        option.setProperty(org.apache.axis2.transport.http.HTTPConstants.COOKIE_STRING, cookie);
+    }
 
-	/**
-	 * Invokes services' writeEventSinkXml method to get All Event Sinks
-	 *
-	 * @param eventSink details of the Event Sink to be written
-	 */
-	public void writeEventSinkXml(EventSink eventSink) throws PublishEventMediatorConfigAdminEventSinkException {
-		try {
-			stub.writeEventSink(eventSink.getName(), eventSink.getUsername(), eventSink.getPassword(),
-			                    eventSink.getReceiverUrlSet(), eventSink.getAuthenticationUrlSet());
-		} catch (RemoteException e) {
-			log.error("Error occurred while wring Event Sink, Error: " + e.getLocalizedMessage());
-		}
-	}
+    /**
+     * Invokes services' writeEventSinkXml method to get All Event Sinks
+     *
+     * @param eventSink details of the Event Sink to be written
+     */
+    public void writeEventSinkXml(EventSink eventSink) throws PublishEventMediatorConfigAdminEventSinkException {
+        try {
+            stub.writeEventSink(eventSink.getName(), eventSink.getUsername(), eventSink.getPassword(),
+                    eventSink.getReceiverUrlSet(), eventSink.getAuthenticationUrlSet());
+        } catch (RemoteException e) {
+            log.error("Error occurred while wring Event Sink, Error: " + e.getLocalizedMessage());
+        }
+    }
 
-	/**
-	 * Invokes services' getAllEventSinks method to get All Event Sinks
-	 *
-	 * @return list of Event Sinks in the deployment directory
-	 */
-	public EventSink[] getAllEventSinks() {
-		EventSink[] eventSinkList = new EventSink[0];
-		try {
-			eventSinkList = stub.getAllEventSinks();
-		} catch (RemoteException e) {
-			log.error("Error Occurred while obtaining list of Event Sinks, Error: " +
-			          e.getLocalizedMessage());
-		}
-		return eventSinkList == null ? new EventSink[0] : eventSinkList;
-	}
+    /**
+     * Invokes services' getAllEventSinks method to get All Event Sinks
+     *
+     * @return list of Event Sinks in the deployment directory
+     */
+    public EventSink[] getAllEventSinks() {
+        EventSink[] eventSinkList = new EventSink[0];
+        try {
+            eventSinkList = stub.getAllEventSinks();
+        } catch (RemoteException e) {
+            log.error("Error Occurred while obtaining list of Event Sinks, Error: " +
+                    e.getLocalizedMessage());
+        }
+        return eventSinkList == null ? new EventSink[0] : eventSinkList;
+    }
 
-	/**
-	 * Invokes services' getEventSinkFromName method to get Specific Event Sink  detail
-	 *
-	 * @return Event Sink corresponds to the given name
-	 */
-	public org.wso2.carbon.event.sink.xsd.EventSink getEventSinkByName(String name)
-			throws PublishEventMediatorConfigAdminEventSinkException {
-		org.wso2.carbon.event.sink.xsd.EventSink eventSink =
-				new org.wso2.carbon.event.sink.xsd.EventSink();
-		try {
-			eventSink = stub.getEventSinkFromName(name);
+    /**
+     * Invokes services' getEventSinkFromName method to get Specific Event Sink  detail
+     *
+     * @return Event Sink corresponds to the given name
+     */
+    public org.wso2.carbon.event.sink.xsd.EventSink getEventSinkByName(String name)
+            throws PublishEventMediatorConfigAdminEventSinkException {
+        org.wso2.carbon.event.sink.xsd.EventSink eventSink =
+                new org.wso2.carbon.event.sink.xsd.EventSink();
+        try {
+            eventSink = stub.getEventSinkFromName(name);
 
-		} catch (RemoteException e) {
-			log.error("Event Sink cannot be deleted, Error: " +
-			          e.getLocalizedMessage());
-		}
-		return eventSink;
-	}
+        } catch (RemoteException e) {
+            log.error("Event Sink cannot be deleted, Error: " +
+                    e.getLocalizedMessage());
+        }
+        return eventSink;
+    }
 
-	/**
-	 * Invokes services' deleteEventSink method to get All Event Sinks
-	 *
-	 * @return status of the deletion as boolean value
-	 */
-	public boolean deleteEventSink(String name) throws PublishEventMediatorConfigAdminEventSinkException {
-		try {
-			return stub.deleteEventSink(name);
-		} catch (RemoteException e) {
-			log.error("Event Sink cannot be deleted, Error: " + e.getLocalizedMessage());
-		}
-		return false;
-	}
+    /**
+     * Invokes services' deleteEventSink method to get All Event Sinks
+     *
+     * @return status of the deletion as boolean value
+     */
+    public boolean deleteEventSink(String name) throws PublishEventMediatorConfigAdminEventSinkException {
+        try {
+            return stub.deleteEventSink(name);
+        } catch (RemoteException e) {
+            log.error("Event Sink cannot be deleted, Error: " + e.getLocalizedMessage());
+        }
+        return false;
+    }
 
     /**
      * Invokes services' updateEventSink method to get All Event Sinks
