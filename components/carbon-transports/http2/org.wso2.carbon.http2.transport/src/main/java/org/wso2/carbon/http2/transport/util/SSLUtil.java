8a8c80196a1b152517726d4cff01084bc9e14fef
chanakadinesh <cdkbalasooriya.13@cse.mrt.ac.lk>
2016-11-04 12:23:28 +0530

    remove unused methods

--- components/carbon-transports/http2/org.wso2.carbon.http2.transport/src/main/java/org/wso2/carbon/http2/transport/util/SSLUtil.java
+++ components/carbon-transports/http2/org.wso2.carbon.http2.transport/src/main/java/org/wso2/carbon/http2/transport/util/SSLUtil.java
@@ -30,84 +30,16 @@ import java.security.*;
 import java.security.cert.CertificateException;
 
 public class SSLUtil {
-    private static String KEY_STORE_TYPE = "JKS";
     private static String TRUST_STORE_TYPE = "JKS";
-    private static String KEY_MANAGER_TYPE = "SunX509";
     private static String TRUST_MANAGER_TYPE = "SunX509";
-    private static String PROTOCOL = "TLS";
-
-    private static SSLContext serverSSLCtx = null;
-    private static SSLContext clientSSLCtx = null;
+    private static TrustManagerFactory trustManagerFactory = null;
 
     private static final Logger LOGGER = Logger.getLogger(SSLUtil.class);
 
-    public static SSLContext createServerSSLContext(final String keyStoreLocation,
-            final String keyStorePwd) {
-        try {
-            if (serverSSLCtx == null) {
-                KeyStore keyStore = KeyStore.getInstance(KEY_STORE_TYPE);
-                keyStore.load(new FileInputStream(keyStoreLocation), keyStorePwd.toCharArray());
-                KeyManagerFactory keyManagerFactory = KeyManagerFactory
-                        .getInstance(KEY_MANAGER_TYPE);
-                keyManagerFactory.init(keyStore, keyStorePwd.toCharArray());
-                serverSSLCtx = SSLContext.getInstance(PROTOCOL);
-                serverSSLCtx.init(keyManagerFactory.getKeyManagers(), null, null);
-            }
-        } catch (UnrecoverableKeyException e) {
-            LOGGER.error("Exception was thrown while building the server SSL Context", e);
-        } catch (KeyManagementException e) {
-            LOGGER.error("Exception was thrown while building the server SSL Context", e);
-        } catch (KeyStoreException e) {
-            LOGGER.error("Exception was thrown while building the server SSL Context", e);
-        } catch (NoSuchAlgorithmException e) {
-            LOGGER.error("Exception was thrown while building the server SSL Context", e);
-        } catch (CertificateException e) {
-            LOGGER.error("Exception was thrown while building the server SSL Context", e);
-        } catch (FileNotFoundException e) {
-            LOGGER.error("Exception was thrown while building the server SSL Context", e);
-        } catch (IOException e) {
-            LOGGER.error("Exception was thrown while building the server SSL Context", e);
-        }
-
-        return serverSSLCtx;
-    }
-
-    public static SSLContext createClientSSLContext(final String trustStoreLocation,
-            final String trustStorePwd) {
-        try {
-            if (clientSSLCtx == null) {
-                KeyStore trustStore = KeyStore.getInstance(TRUST_STORE_TYPE);
-                trustStore
-                        .load(new FileInputStream(trustStoreLocation), trustStorePwd.toCharArray());
-                TrustManagerFactory trustManagerFactory = TrustManagerFactory
-                        .getInstance(TRUST_MANAGER_TYPE);
-                trustManagerFactory.init(trustStore);
-                clientSSLCtx = SSLContext.getInstance(PROTOCOL);
-                clientSSLCtx.init(null, trustManagerFactory.getTrustManagers(), null);
-            }
-        } catch (KeyManagementException e) {
-            LOGGER.error("Exception was thrown while building the client SSL Context", e);
-        } catch (KeyStoreException e) {
-            LOGGER.error("Exception was thrown while building the client SSL Context", e);
-        } catch (NoSuchAlgorithmException e) {
-            LOGGER.error("Exception was thrown while building the client SSL Context", e);
-        } catch (CertificateException e) {
-            LOGGER.error("Exception was thrown while building the client SSL Context", e);
-        } catch (FileNotFoundException e) {
-            LOGGER.error("Exception was thrown while building the client SSL Context", e);
-        } catch (IOException e) {
-            LOGGER.error("Exception was thrown while building the client SSL Context", e);
-        }
-
-        return clientSSLCtx;
-
-    }
-
     public static TrustManagerFactory createTrustmanager(final String trustStoreLocation,
             final String trustStorePwd) {
-        TrustManagerFactory trustManagerFactory = null;
         try {
-            if (clientSSLCtx == null) {
+            if (trustManagerFactory == null) {
                 KeyStore trustStore = KeyStore.getInstance(TRUST_STORE_TYPE);
                 trustStore
                         .load(new FileInputStream(trustStoreLocation), trustStorePwd.toCharArray());
@@ -115,15 +47,15 @@ public class SSLUtil {
                 trustManagerFactory.init(trustStore);
             }
         } catch (KeyStoreException e) {
-            LOGGER.error("Exception was thrown while building the client SSL Context", e);
+            LOGGER.error("Exception was thrown while building the client SSL context", e);
         } catch (NoSuchAlgorithmException e) {
-            LOGGER.error("Exception was thrown while building the client SSL Context", e);
+            LOGGER.error("Exception was thrown while building the client SSL context", e);
         } catch (CertificateException e) {
-            LOGGER.error("Exception was thrown while building the client SSL Context", e);
+            LOGGER.error("Exception was thrown while building the client SSL context", e);
         } catch (FileNotFoundException e) {
-            LOGGER.error("Exception was thrown while building the client SSL Context", e);
+            LOGGER.error("Exception was thrown while building the client SSL context", e);
         } catch (IOException e) {
-            LOGGER.error("Exception was thrown while building the client SSL Context", e);
+            LOGGER.error("Exception was thrown while building the client SSL context", e);
         }
         return trustManagerFactory;
     }
