0a34f3be6b603a141fb215d03e2a5ff5eecc2429
chanakadinesh <cdkbalasooriya.13@cse.mrt.ac.lk>
2016-11-04 11:26:10 +0530

    remove unused dataframe sending in http2 TransportSender

--- components/carbon-transports/http2/org.wso2.carbon.http2.transport/src/main/java/org/wso2/carbon/http2/transport/Http2TransportSender.java
+++ components/carbon-transports/http2/org.wso2.carbon.http2.transport/src/main/java/org/wso2/carbon/http2/transport/Http2TransportSender.java
@@ -1,3 +1,21 @@
+/*
+ *   Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *   WSO2 Inc. licenses this file to you under the Apache License,
+ *   Version 2.0 (the "License"); you may not use this file except
+ *   in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
 package org.wso2.carbon.http2.transport;
 
 import io.netty.buffer.Unpooled;
@@ -27,7 +45,6 @@ import org.apache.synapse.transport.passthru.config.TargetConfiguration;
 import org.apache.synapse.transport.passthru.util.RelayUtils;
 import org.wso2.carbon.http2.transport.util.Http2ClientHandler;
 import org.wso2.carbon.http2.transport.util.Http2ConnectionFactory;
-import org.wso2.carbon.http2.transport.util.Http2Constants;
 
 import javax.xml.stream.XMLStreamException;
 import java.io.IOException;
@@ -76,9 +93,9 @@ public class Http2TransportSender extends AbstractTransportSender {
             if (port == -1) {
                 // use default
                 if ("http".equals(scheme)) {
-                    port = 80;
+                    port = 8080;
                 } else if ("https".equals(scheme)) {
-                    port = 443;
+                    port = 8083;
                 }
             }
             HttpHost target = new HttpHost(hostname, port, scheme);
@@ -93,70 +110,53 @@ public class Http2TransportSender extends AbstractTransportSender {
             Http2ClientHandler clientHandler = connectionFactory.getChannelHandler(uri);
 
             clientHandler.setTargetConfig(targetConfiguration);
-            //For steaming data
-            int streamId;
-            if (msgCtx.getProperty(Http2Constants.HTTP2_DATA_FRAME_PRESENT) != null && msgCtx
-                    .getProperty(Http2Constants.HTTP2_DATA_FRAME_PRESENT).equals(true)) {
-                Http2DataFrame frame = (Http2DataFrame) msgCtx
-                        .getProperty(Http2Constants.HTTP2_DATA_FRAME);
-                if (log.isDebugEnabled()) {
-                    log.debug("Sending the data frame to the Http2 server on channel id : "
-                            + clientHandler.getChannel().toString());
-                }
-                streamId = clientHandler.getStreamId();
-                Channel channel = clientHandler.getChannel();
-                if (channel.isActive()) {
-                    clientHandler.setRequest(streamId, msgCtx);
-                    clientHandler.put(streamId, frame.retain());
-                }
-            } else {
-                RelayUtils.buildMessage(msgCtx, false);
-                OMOutputFormat format = BaseUtils.getOMOutputFormat(msgCtx);
-                MessageFormatter messageFormatter = MessageProcessorSelector
-                        .getMessageFormatter(msgCtx);
-                StringWriter sw = new StringWriter();
-                OutputStream out = new WriterOutputStream(sw, format.getCharSetEncoding());
-                messageFormatter.writeTo(msgCtx, format, out, true);
-                String contentType = messageFormatter
-                        .getContentType(msgCtx, format, msgCtx.getSoapAction());
-                out.close();
-                String msg = sw.toString();
 
-                if (log.isDebugEnabled()) {
-                    log.debug("Sending the default request to the Http2 server on context id : "
-                            + clientHandler.getChannel().toString());
-                }
-                streamId = clientHandler.getStreamId();
-                Channel channel = clientHandler.getChannel();
+            RelayUtils.buildMessage(msgCtx, false);
+            OMOutputFormat format = BaseUtils.getOMOutputFormat(msgCtx);
+            MessageFormatter messageFormatter = MessageProcessorSelector
+                    .getMessageFormatter(msgCtx);
+            StringWriter sw = new StringWriter();
+            OutputStream out = new WriterOutputStream(sw, format.getCharSetEncoding());
+            messageFormatter.writeTo(msgCtx, format, out, true);
+            String contentType = messageFormatter
+                    .getContentType(msgCtx, format, msgCtx.getSoapAction());
+            out.close();
+            String msg = sw.toString();
+
+            int streamId = clientHandler.getStreamId();
+            Channel channel = clientHandler.getChannel();
+
+            if (log.isDebugEnabled()) {
                 log.debug("Channel created to send message");
-                if (channel.isActive()) {
-                    clientHandler.setRequest(streamId, msgCtx);
-                    log.debug("Sending message to backend: " + msg);
-                    String method = (
-                            msgCtx.getProperty(Constants.Configuration.HTTP_METHOD) != null ?
-                                    msgCtx.getProperty(Constants.Configuration.HTTP_METHOD)
-                                            .toString() :
-                                    POST.toString());
-                    //Set content type and required frames
-                    HttpMethod m = new HttpMethod(method);
-                    FullHttpRequest request = new DefaultFullHttpRequest(HTTP_1_1, m, uri.getPath(),
-                            Unpooled.copiedBuffer(msg.getBytes(CharsetUtil.UTF_8)));
-                    request.headers().add(HttpHeaderNames.HOST, new URI(targetEPR).getHost());
-                    request.headers().add(HttpConversionUtil.ExtensionHeaderNames.SCHEME.text(),
-                            secure ? HttpScheme.HTTPS : HttpScheme.HTTP);
-                    request.headers().add(HttpHeaderNames.CONTENT_TYPE, contentType);
-                    request.headers().add(HttpHeaderNames.ACCEPT_ENCODING, HttpHeaderValues.GZIP);
-                    request.headers()
-                            .add(HttpHeaderNames.ACCEPT_ENCODING, HttpHeaderValues.DEFLATE);
-                    clientHandler.put(streamId, request);
-                    log.info("Request sent to backend with stream id:" + streamId);
-                }
             }
 
+            if (channel.isActive()) {
+                clientHandler.setRequest(streamId, msgCtx);
+                if (log.isDebugEnabled()) {
+                    log.debug("Sending message to backend: " + msg);
+                }
+                String method = (msgCtx.getProperty(Constants.Configuration.HTTP_METHOD) != null ?
+                        msgCtx.getProperty(Constants.Configuration.HTTP_METHOD).toString() :
+                        POST.toString());
+                //Set content type and required frames
+                HttpMethod m = new HttpMethod(method);
+                FullHttpRequest request = new DefaultFullHttpRequest(HTTP_1_1, m, uri.getPath(),
+                        Unpooled.copiedBuffer(msg.getBytes(CharsetUtil.UTF_8)));
+                request.headers().add(HttpHeaderNames.HOST, new URI(targetEPR).getHost());
+                request.headers().add(HttpConversionUtil.ExtensionHeaderNames.SCHEME.text(),
+                        secure ? HttpScheme.HTTPS : HttpScheme.HTTP);
+                request.headers().add(HttpHeaderNames.CONTENT_TYPE, contentType);
+                request.headers().add(HttpHeaderNames.ACCEPT_ENCODING, HttpHeaderValues.GZIP);
+                request.headers().add(HttpHeaderNames.ACCEPT_ENCODING, HttpHeaderValues.DEFLATE);
+                clientHandler.put(streamId, request);
+                if (log.isDebugEnabled()) {
+                    log.debug("Request sent to backend with stream id:" + streamId);
+                }
+            }
         } catch (URISyntaxException e) {
-            log.error("Error parsing the WS endpoint url", e);
+            log.error("Error parsing the http2 endpoint url", e);
         } catch (IOException e) {
-            log.error("Error writting to the websocket channel", e);
+            log.error("Error writting to the http2 channel", e);
         } catch (XMLStreamException e) {
             handleException("Error while building message", e);
         }
--- components/carbon-transports/http2/org.wso2.carbon.http2.transport/src/main/java/org/wso2/carbon/http2/transport/util/Http2Constants.java
+++ components/carbon-transports/http2/org.wso2.carbon.http2.transport/src/main/java/org/wso2/carbon/http2/transport/util/Http2Constants.java
@@ -22,12 +22,6 @@ public class Http2Constants {
     public static final String HTTP2 = "http2";
     public static final String HTTPS2 = "https2";
 
-    public static final String HTTP2_HEADER_FRAME_PRESENT = "http2.frame.header.present";
-    public static final String HTTP2_DATA_FRAME_PRESENT = "http2.frame.data.present";
-
-    public static final String HTTP2_HEADER_FRAME = "http2.frame.header";
-    public static final String HTTP2_DATA_FRAME = "http2.frame.data";
-
     public static final String TRUST_STORE_LOCATION = "h2.trust.store.location";
     public static final String TRUST_STORE_PASSWORD = "h2.trust.store.Password";
     public static final String TRUST_STORE_CONFIG_ELEMENT = "h2.trust.store";
