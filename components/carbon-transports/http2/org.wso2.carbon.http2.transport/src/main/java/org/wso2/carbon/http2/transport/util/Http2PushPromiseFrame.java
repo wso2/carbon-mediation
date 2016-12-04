113be99a87ab3b4e750a85c7e4927342eb57c2e5
chanakadinesh <cdkbalasooriya.13@cse.mrt.ac.lk>
2016-12-04 09:55:51 +0530

    implement pushpromise frame

--- components/carbon-transports/http2/org.wso2.carbon.http2.transport/src/main/java/org/wso2/carbon/http2/transport/util/Http2PushPromiseFrame.java
+++ components/carbon-transports/http2/org.wso2.carbon.http2.transport/src/main/java/org/wso2/carbon/http2/transport/util/Http2PushPromiseFrame.java
@@ -1,8 +1,47 @@
 package org.wso2.carbon.http2.transport.util;
 
+import io.netty.handler.codec.http2.Http2Headers;
+import io.netty.handler.codec.http2.Http2StreamFrame;
+
 /**
  * Created by chanakabalasooriya on 12/3/16.
  */
-public class Http2PushPromiseFrame {
+public class Http2PushPromiseFrame implements Http2StreamFrame{
+    int streamId;
+    int pushPromiseId;
+    Http2Headers headers;
 
+    @Override
+    public Http2StreamFrame setStreamId(int i) {
+        streamId=i;
+        return this;
+    }
+
+    @Override
+    public int streamId() {
+        return streamId;
+    }
+
+    @Override
+    public String name() {
+        return "PushPromiseFrame";
+    }
+
+    public int getPushPromiseId() {
+        return pushPromiseId;
+    }
+
+    public Http2StreamFrame setPushPromiseId(int pushPromiseId) {
+        this.pushPromiseId = pushPromiseId;
+        return this;
+    }
+
+    public Http2Headers getHeaders() {
+        return headers;
+    }
+
+    public Http2StreamFrame setHeaders(Http2Headers headers) {
+        this.headers = headers;
+        return this;
+    }
 }
