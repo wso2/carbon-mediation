4ed5768a47491a8340af5f5435c76123e689b91c
Ibaqu <aquib@wso2.com>
2019-08-15 10:20:50 +0530

    Changed how endpoint test displays Error Messages

--- components/mediation-ui/org.wso2.carbon.endpoint.ui/src/main/java/org/wso2/carbon/endpoint/ui/util/EndpointConfigurationHelper.java
+++ components/mediation-ui/org.wso2.carbon.endpoint.ui/src/main/java/org/wso2/carbon/endpoint/ui/util/EndpointConfigurationHelper.java
@@ -145,7 +145,7 @@ public class EndpointConfigurationHelper {
             } catch (SSLHandshakeException e) {
                 returnValue = "ssl_error";
             } catch (Exception e) {
-                returnValue = "Cannot establish connection to the provided address";
+                returnValue = "Error thrown when connecting to endpoint : " + e.getMessage();
             }
         }
         return returnValue;
