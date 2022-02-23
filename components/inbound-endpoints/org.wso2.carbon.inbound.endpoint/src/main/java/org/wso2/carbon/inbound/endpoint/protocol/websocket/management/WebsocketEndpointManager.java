491a834c512a95da7e4a1262a661212ac399c4e7
Vidura Nanayakkara <viduran@viduran.local>
2022-02-23 17:20:14 +0530

    Fix possible NPE

--- components/inbound-endpoints/org.wso2.carbon.inbound.endpoint/src/main/java/org/wso2/carbon/inbound/endpoint/protocol/websocket/management/WebsocketEndpointManager.java
+++ components/inbound-endpoints/org.wso2.carbon.inbound.endpoint/src/main/java/org/wso2/carbon/inbound/endpoint/protocol/websocket/management/WebsocketEndpointManager.java
@@ -258,10 +258,10 @@ public class WebsocketEndpointManager extends AbstractInboundEndpointManager {
                         InboundWebsocketConstants.CUSTOM_SEQUENCE))
                 .usePortOffset(Boolean.valueOf(params.getProperties().getProperty(
                         InboundWebsocketConstants.WEBSOCKET_USE_PORT_OFFSET)))
-                .inflowIdleTime(Integer.valueOf(params.getProperties()
-                        .getProperty(InboundWebsocketConstants.WEBSOCKET_DISPATCH_IDLETIME)))
-                .outflowIdleTime(Integer.valueOf(params.getProperties()
-                        .getProperty(InboundWebsocketConstants.WEBSOCKET_OUTFLOW_DISPATCH_IDLETIME)))
+                .inflowIdleTime(Integer.parseInt(params.getProperties()
+                        .getProperty(InboundWebsocketConstants.WEBSOCKET_DISPATCH_IDLETIME, "0")))
+                .outflowIdleTime(Integer.parseInt(params.getProperties()
+                        .getProperty(InboundWebsocketConstants.WEBSOCKET_OUTFLOW_DISPATCH_IDLETIME, "0")))
                 .build();
     }
 
