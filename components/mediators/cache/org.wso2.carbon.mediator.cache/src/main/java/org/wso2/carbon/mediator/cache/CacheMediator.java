ea4b8738ec9203affebe86bb8f93b521cbea8e4b
Prasanna Dangalla <prasannadangalla@gmail.com>
2022-11-29 12:13:28 +0530

    Fixed the conflicts in https://github.com/wso2/carbon-mediation/pull/1650 .

--- components/mediators/cache/org.wso2.carbon.mediator.cache/src/main/java/org/wso2/carbon/mediator/cache/CacheMediator.java
+++ components/mediators/cache/org.wso2.carbon.mediator.cache/src/main/java/org/wso2/carbon/mediator/cache/CacheMediator.java
@@ -477,11 +477,10 @@ public class CacheMediator extends AbstractMediator implements ManagedLifecycle,
                 }
             }
             if (toCache) {
-                if (JsonUtil.hasAJsonPayload(msgCtx)) {
-                	String contentType = ((String) msgCtx.getProperty(Constants.Configuration.CONTENT_TYPE));
-                	if (contentType != null) {
-                    		contentType = contentType.split(";")[0];
-                	}
+                String contentType = ((String) msgCtx.getProperty(Constants.Configuration.CONTENT_TYPE));
+                if (contentType != null) {
+                        contentType = contentType.split(CachingConstants.PROPERTY_SEPARATOR)[0];
+                }
                 if (contentType != null && contentType.equals(jsonContentType)) {
                     byte[] responsePayload = JsonUtil.jsonPayloadToByteArray(msgCtx);
                     if (response.getMaxMessageSize() > -1 &&
