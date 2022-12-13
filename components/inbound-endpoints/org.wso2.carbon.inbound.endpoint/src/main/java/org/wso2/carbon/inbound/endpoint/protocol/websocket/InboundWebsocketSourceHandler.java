44c94f7187248c265971365c99bc4cc15d2f4cdc
yasasrangika <rangikaymendis@gmail.com>
2022-12-13 20:18:35 +0530

    fix close connection issue

--- components/inbound-endpoints/org.wso2.carbon.inbound.endpoint/src/main/java/org/wso2/carbon/inbound/endpoint/protocol/websocket/InboundWebsocketSourceHandler.java
+++ components/inbound-endpoints/org.wso2.carbon.inbound.endpoint/src/main/java/org/wso2/carbon/inbound/endpoint/protocol/websocket/InboundWebsocketSourceHandler.java
@@ -684,6 +684,9 @@ public class InboundWebsocketSourceHandler extends ChannelInboundHandlerAdapter 
                 .setProperty(InboundWebsocketConstants.WEBSOCKET_SOURCE_HANDLER_CONTEXT, wrappedContext.getChannelHandlerContext());
         synCtx.setProperty(InboundWebsocketConstants.WEBSOCKET_SOURCE_CHANNEL_IDENTIFIER,
                 wrappedContext.getChannelIdentifier());
+        ((Axis2MessageContext) synCtx).getAxis2MessageContext()
+                .setProperty(InboundWebsocketConstants.WEBSOCKET_SOURCE_CHANNEL_IDENTIFIER,
+                        wrappedContext.getChannelIdentifier());
         synCtx.setProperty(InboundWebsocketConstants.WEBSOCKET_CORRELATION_ID, wrappedContext.getChannelIdLongText());
         ((Axis2MessageContext) synCtx).getAxis2MessageContext()
                 .setProperty(InboundWebsocketConstants.WEBSOCKET_CORRELATION_ID, wrappedContext.getChannelIdLongText());
